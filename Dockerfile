8edf618ecca916bf4d9ad107b6cc2511481b9856
Zachary Noah <zacharybnoah@gmail.com>
2019-01-30 18:22:21 -0800

    requests is failing me, trying with wget module

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,15 @@
-# see https://docs.docker.com/docker-cloud/builds/advanced/
-# using ARG in FROM requires min v17.05.0-ce
-ARG QGIS_DOCKER_TAG=latest
+FROM qgis/qgis:latest
+ENV CACHEBUSTER=123
 
-FROM  qgis/qgis:${QGIS_DOCKER_TAG}
 MAINTAINER Denis Rouzaud <denis@opengis.ch>
 
 RUN curl https://bootstrap.pypa.io/get-pip.py | python3
-RUN pip3 install sphinx
+RUN pip3 install sphinx requests
 
 COPY NED_Reference/ /NED_Reference
 COPY . /
 
 RUN chmod 777 -R ./NED_Reference
-RUN ls
-RUN cd NED_Reference && ls
+
 
 ENTRYPOINT ["python3", "dem.py"]
--- dem.py
+++ dem.py
@@ -1,37 +1,80 @@
 from osgeo import ogr
 import os
 import logging
+import requests
+# import zipfile
+
+
+def getBoxes(shapefile_path="./test_shape/test_shape.shp"):
+
+    driver = ogr.GetDriverByName("ESRI Shapefile")
+
+    box_shapefile = "./NED_Reference/ned_13arcsec_g.shp"
+    input_shapefile = shapefile_path
+
+    box_datasource = driver.Open(box_shapefile, 0)
+    box_layer = box_datasource.GetLayer()
+
+    input_datasource = driver.Open(input_shapefile, 0)
+    input_layer = input_datasource.GetLayer()
+
+    input_feature = input_layer.GetNextFeature()
+    input_geom = input_feature.GetGeometryRef()
+
+    selections = []
+    for box_feature in box_layer:
+        box_geom = box_feature.GetGeometryRef()
+        if (box_geom.Intersects(input_geom)):
+            selections.append(box_feature.GetField("FILE_ID"))
+
+    return selections
+
+
+def make_links(boxid_list):
+    link_list = []
+    for box_id in boxid_list:
+        link = "https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/13/ArcGrid/USGS_NED_13_{}_ArcGrid.zip".format(box_id)
+        link_list.append({"link": link, "box_id": box_id})
+    return link_list
+
+def download_files(link_list):
+    chunk_size = 8096
+    bytes_transferred = 0
+
+    for link in link_list:
+        logging.warning("Now downloading file {current_file} of {number_of_files}".format(current_file=link_list.index(link) + 1, number_of_files=len(link_list)))
+        url = link["link"]
+        r = requests.get('https://placekitten.com/200/300')
+        with open('./data/{}.jpg'.format(link["box_id"]), 'wb') as f:
+            f.write(r.content)
+
+        #     # total_length = int(r.headers.get('content-length'))
+        #     for chunk in r.iter_content(chunk_size):
+        #         if chunk:
+        #             f.write(chunk)
+                # bytes_transferred += len(chunk)
+                # percentage = bytes_transferred/total_length * 100
+                # logging.warning("=====percentage=====")
+                # logging.warning(percentage)
+        # So it only downloads one file
+            # f.write(r.content)
+        break
+
+boxid_list = getBoxes()
+logging.warning(boxid_list)
+link_list = make_links(boxid_list)
+logging.warning(link_list)
+download_files(link_list)
 
 
 
-driver = ogr.GetDriverByName("ESRI Shapefile")
-
-box_shapefile = "./NED_Reference/ned_13arcsec_g.shp"
-california_shapefile = "./CA_state_reproject.shp"
-
-box_datasource = driver.Open(box_shapefile, 0)
-box_layer = box_datasource.GetLayer()
-
-california_datasource = driver.Open(california_shapefile, 0)
-california_layer = california_datasource.GetLayer()
-
-california_feature = california_layer.GetNextFeature()
-california_geom = california_feature.GetGeometryRef()
-
-selections = []
-for box_feature in box_layer:
-    box_geom = box_feature.GetGeometryRef()
-    if (box_geom.Intersects(california_geom)):
-        logging.warning(box_feature.GetField("FILE_ID"))
-    # for california_feature in california_layer:
-    #     california_geom = california_feature.GetGeometryRef()
-    #     if (box_geom.Intersects(california_geom)):
-    #         FILE_ID = box_feature.GetField("FILE_ID")
-    #         if FILE_ID not in selections:
-    #             selections.append(FILE_ID)
-    #     logging.warning("====Below did not intersect=====")
-    #     logging.warning(box_feature.GetField("FILE_ID"))
 
 
-
-logging.warning(selections)
+# r = requests.get(test_url["link"], stream=True)
+# path = './data/{}'.format(test_url["box_id"])
+# with open(path, 'wb') as f:
+#     total_length = int(r.headers.get('content-length'))
+#     for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
+#         if chunk:
+#             f.write(chunk)
+#             f.flush()
